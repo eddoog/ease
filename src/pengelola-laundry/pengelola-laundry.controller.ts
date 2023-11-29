c7046034f5c9d554213d7bd3f2855e6f14ea9760
bagas-shalahuddin <bagas.shalahuddin@ui.ac.id>
2023-11-29 15:16:41 +0700

    feat : implement createPenilaian

--- src/pengelola-laundry/pengelola-laundry.controller.ts
+++ src/pengelola-laundry/pengelola-laundry.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  UseGuards,
+  Post,
+  Body,
+  BadRequestException,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Role } from 'src/common';
 import { JwtAuthGuard, RolesGuard } from 'src/guard';
 import { PengelolaLaundryService } from './pengelola-laundry.service';
+import { CreatePenilaianDTO } from './dto/create-penilaian.dto';
+import { Roles } from 'src/decorator';
 
 @ApiTags('pengelola-laundry')
 @Controller('pengelola-laundry')
@@ -10,4 +19,22 @@ export class PengelolaLaundryController {
   constructor(
     private readonly pengelolaLaundryService: PengelolaLaundryService,
   ) {}
+
+  @Post('create-penilaian')
+  @Roles(Role.PELANGGAN)
+  async createPenilaian(
+    @Body() createPenilaianDTO: CreatePenilaianDTO,
+  ) {
+    if (!!!createPenilaianDTO.rating) {
+      throw new BadRequestException('Rating tidak boleh kosong');
+    }
+
+    if (createPenilaianDTO.ulasan.length > 255) {
+      throw new BadRequestException(
+        'Ulasan terlalu panjang (max 255 karakter)',
+      );
+    }
+
+    return this.pengelolaLaundryService.createPenilaian(createPenilaianDTO);
+  }
 }
